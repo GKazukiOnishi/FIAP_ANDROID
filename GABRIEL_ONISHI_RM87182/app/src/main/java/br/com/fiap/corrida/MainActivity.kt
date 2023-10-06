52241ee90daac1eb21020203c3f3e1102da8bb82
GKazukiOnishi <kazuki.onishi@hotmail.com>
2023-10-06 00:03:03 -0300

    Projeto Ckp 5 Android - Corrida de Cavalos

---
+++ GABRIEL_ONISHI_RM87182/app/src/main/java/br/com/fiap/corrida/MainActivity.kt
@@ -0,0 +1,16 @@
+package br.com.fiap.corrida
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fiap.corrida.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
